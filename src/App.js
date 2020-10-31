0f33ed0cc6ee7e02f593ca47958a37b99e9fabb9
Arjun Singh <arjun.oct.2003@outlook.com>
2020-10-31 08:53:36 +0530

    Updated Readme.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import "./App.css";
 
+import React from "react";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Works from "./pages/Works";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" exact component={About} />
-      <Route path="/works" exact component={Works} />
-    </Router>
+    <React.Fragment>
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/works" exact component={Works} />
+
+        <nav>
+          <h1>
+            <Link to="/">Home</Link>
+          </h1>
+          <h1>
+            <Link to="/about">About</Link>
+          </h1>
+          <h1>
+            <Link to="/works">Works</Link>
+          </h1>
+        </nav>
+      </Router>
+    </React.Fragment>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,58 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import * as THREE from "three";
+
+import "./styles/Home.css";
 
 class Home extends Component {
+  componentDidMount() {
+    this.renderCube();
+  }
   render() {
     return (
-      <div class="page">
+      <div className="page">
         <h1>This is the homepage</h1>
-        <Link to="/about">About</Link>
+        <div className="container"></div>
       </div>
     );
+  }
+
+  renderCube() {
+    const container = document.querySelector("div.container");
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      container.clientWidth / container.clientHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(container.clientWidth, container.clientHeight);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    container.appendChild(renderer.domElement);
+
+    function render() {
+      requestAnimationFrame(render);
+      renderer.render(scene, camera);
+
+      cube.rotation.y += 0.1;
+    }
+
+    window.addEventListener("resize", () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(container.clientWidth, container.clientHeight);
+    });
+
+    render();
   }
 }
 
